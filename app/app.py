014847c71d9f366f845ccd9036da5de828bdd83c
Vito Tafuni <vito.tafuni@apuliasoft.com>
2019-03-19 18:06:40 +0100

    personal access token management from config file openreq lib integration error messages format standardization _issuesToRequirements function to bridge from git issue to openreq requirements train function complete and functional various test endpoint to check functionality

--- app/app.py
+++ app/app.py
@@ -6,6 +6,7 @@ from flask import Flask, request, jsonify, redirect, session, url_for
 import os
 from openreq import OpenReq
 from github import GitApp
+import time
 
 api = Flask(__name__)
 api.config.from_pyfile('config.py', silent=True)
@@ -14,9 +15,11 @@ git = GitApp(
   api.config['APP_ID'], 
   api.config['CLIENT_ID'], 
   api.config['CLIENT_SECRET'], 
-  os.path.join(os.path.dirname(__file__), "private-key.pem")
+  os.path.join(os.path.dirname(__file__), "private-key.pem"),
+  PERSONAL_ACCESS_TOKEN = api.config['PERSONAL_ACCESS_TOKEN'] 
 )
 
+opnr = OpenReq(api.config['OPENREQ_BASEURL'])
 
 def authorized(f):
   @wraps(f)
@@ -44,7 +47,9 @@ def auth(next):
     session['user_id'] = user['login']
     return redirect(url_for('index', _external=True) + next)
   else: 
-    return "Invalid authorization",401
+    return jsonify({
+      'message': "Invalid authorization"
+    }),401
 
 
 @api.route("/")
@@ -81,11 +86,22 @@ def install():
   """
   return redirect(git.appPageUrl)
 
+
+def _issuesToRequirements(issues):
+  return [ { 
+    'id': issue['node_id'] + label['node_id'],
+    'reqDomains': "",
+    'requirement_type': label['name'],
+    'text': issue['body']
+  } for issue in issues if 'pull_request' not in issue for label in issue['labels'] ]
+
+
 @api.route('/train')
 @authorized
 def train(token):
+  start = time.time()
   """
-    Train a model from a repository using OpenReq API
+    Train a model from a GIT repository using OpenReq API
     
     Parameters:
       repo  (string): a repository in the form owner/repo
@@ -95,15 +111,25 @@ def train(token):
   """
   repo = request.args['repo']
   company, property = repo.split("/")
+  
   # check if repo has been trained already
-  if OpenReq.exists(company, property):
+  if not opnr.exists(company, property):
     # retrieve issues for repo
     issues = git.getIssues(repo)
+    # convert github issues to requirement
+    requirements = _issuesToRequirements(issues)
     # call openreq api to train issues retrived
-    OpenReq.trainModel(company, property, issues)
-    
-  # associate trained model to user
-  return "OK" # TODO define return form 
+    opnr.train(company, property, requirements)
+  else:
+    requirements = []
+  
+  # TODO associate trained model to user
+  
+  return jsonify({
+    'message': "Model has been trained and associated to your userbase.",
+    'requirements': len(requirements),
+    'time': time.time() - start
+  })
 
 
 @api.route('/classify')
@@ -125,16 +151,20 @@ def classify(token):
   # check repo if user has installed app on
   if git.isInstalled(repo) :
     # check model if exists
-    if not OpenReq.checkModelexists(company, property):
-      return "Train the model before you can use it", 404
+    if not opnr.exists(company, property):
+      return jsonify({
+        'message': "Train the model before you can use it"
+      }), 404
     # retrieve issues for repo
     # for each issue call openreq api to classify repo based on model
     for issue in git.getIssues(repo):
-      labels = OpenReq.classifyIssue(company, property, issue)
+      labels = opnr.classify(company, property, issue)
       #   write labels for issue
       git.setLabels(issue,labels)
   else:
-    return "App not installed on this repository", 401
+    return jsonify({
+      'message': "App not installed on this repository"
+    }), 401
   
   return "OK" # TODO define return form 
 
@@ -146,28 +176,60 @@ def webhook():
   
 # test endpoints
 
+@api.route("/limit")
+@authorized
+def limit(token):
+  import requests
+  
+  limits = {}
+  
+  r = requests.get('https://api.github.com/rate_limit')
+  limits['no_token'] = r.json()
+  
+  r = requests.get('https://api.github.com/rate_limit', headers={'Authorization': 'token ' + token})
+  limits['auth_token'] = r.json()
+  
+  return jsonify(limits)
+  #return jsonify([ x['full_name'] for x in r.json() ])
+
 @api.route("/myrepos")
 @authorized
 def myrepos(token):
-  r = requests.get('https://api.github.com/users/{username}/repos'.format(username = session['user_id']), headers={'Authorization': 'token ' + token})
-  #return jsonify(r.json())
-  return jsonify([ x['full_name'] for x in r.json() ])
+  import requests
+  r = requests.get('https://api.github.com/app/installation/repositories'.format(username = session['user_id']), headers=git.jwtHeader)
+  return jsonify(r.json())
+  #return jsonify([ x['full_name'] for x in r.json() ])
 
+@api.route("/exists")
+@authorized
+def exists(token):
+  repo = request.args['repo']
+  company, property = repo.split("/")
+  return jsonify(opnr.exists(company, property))
 
-@api.route("/issues/<path:repo>")
-def issues(repo):
-  return jsonify(git.getIssues(repo))
+@api.route("/issues")
+@authorized
+def issues(token):
+  repo = request.args['repo']
+
+  #return jsonify(git.getIssues(repo, token))
+  return jsonify(git.getIssues(repo, git.PERSONAL_ACCESS_TOKEN))
+  #return jsonify({'requirements': _issuesToRequirements(git.getIssues(repo))})
 
 @api.route("/login")
 @authorized
 def login(token):
   return jsonify(git.getUser(token))
 
-@api.route("/test/<path:repo>")
+@api.route("/test")
 @authorized
-def test(token, repo):
-  issues = git.getIssues(repo)
-  return jsonify(issues)
+def test(token):
+  repo = request.args['repo']
+  model = request.args['model'] 
+  company, property = model.split("/")
+  requirements = _issuesToRequirements(git.getIssues(repo))
+  r = opnr.classify(company, property, requirements[0:1])
+  return jsonify(r.json())
 
 
 
